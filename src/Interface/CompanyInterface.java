568983ac5d617ee9270f7e5123cf8b1ec21f5d7f
005Dikshant <005dikshant@gmail.com>
2023-01-29 19:18:31 -0500

    Change-3 |Add Bill, Issue cheques added in interface && file Name changed from 'Main' to 'Company'| Dikshant

---
+++ src/Interface/CompanyInterface.java
@@ -0,0 +1,23 @@
+/*
+* @Author: Dikshant Yadav
+* Student ID: 40221623
+*  
+* @Author: Vinayak Sareen
+* Student ID: 40186182
+* */
+
+package Interface;
+import Models.Bill;
+import Models.Employee;
+import java.util.*;
+
+// Main Interface and Required functionalities
+public interface CompanyInterface {
+	public Employee findEmployeeByID(Integer employeeID);
+    public Employee findEmployee(Object object);
+    public void displayMenu(Scanner snc);
+    public Employee addEmployee(Scanner snc);
+    public Bill addBill(Scanner snc);
+    public void issueChecques();
+    
+}
